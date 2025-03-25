304a46908781dfd5fdbe76dbff3437b0fd10e9f0
NoobMahbub <Mahbubbengali@gmail.com>
2025-03-25 18:01:34 +0600

    consumed the counter properties and send payload using incrementbyamount action

---
+++ src/features/counter/CounterView.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment, incrementByAmount } from "./counterSlice";
+
+const CounterView = () => {
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+  console.log(count);
+  return (
+    <div>
+      <h2>Count : {count}</h2>
+      <button onClick={()=>dispatch(incrementByAmount(5))}>increment by 5</button>
+      <button onClick={()=>dispatch(increment())}>increment</button>
+      <button onClick={()=>dispatch(decrement())}>decrement</button>
+    </div>
+  );
+};
+
+export default CounterView;
