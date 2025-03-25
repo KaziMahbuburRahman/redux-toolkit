8da55e1c335fa6b6a5f12488d269f853126c52aa
NoobMahbub <Mahbubbengali@gmail.com>
2025-03-25 18:01:53 +0600

    react poject setup done

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import CounterView from "./features/counter/counterView";
+
+const App = () => {
+  return (
+    <div>
+      <CounterView />
+    </div>
+  );
+};
+
+export default App;
